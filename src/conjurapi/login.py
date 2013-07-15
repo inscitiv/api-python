a12473dabdb55e701d1616c5b22a40773cf58025
bernzbroad <dbernick@broadinstitute.org>
2013-07-15 17:06:05 -0400

    Made fixes to make .conjurrc compatible with CLI conjurrc (ie, make it YAML compatible).

--- src/conjurapi/login.py
+++ src/conjurapi/login.py
@@ -3,99 +3,121 @@ Created on Jul 7, 2013
 
 @author: bernz
 '''
-import ConfigParser,caslib
-import os.path
-import httplib, urllib, urllib2, cookielib
+import ConfigParser,caslib,os.path,httplib, urllib, urllib2, cookielib,base64
 from restkit import Resource
+from yaml import load
 
 class ConjurConfig:
-	
-	def getAccount(self):
-		config = ConfigParser.ConfigParser()
-		try:
-			config.read(os.path.expanduser("~")+'/.conjurrc')
-			return config.get('Conjur', 'account')[0]
-		except Exception,e:
-			config.read('conjur.py.example')
-			return config.get('Conjur', 'account')[0]
+    
+    def getAccount(self):
+        #config = ConfigParser.ConfigParser()
+        try:
+            config = load(file(os.path.expanduser("~")+'/.conjurrc','r'))
+            #config = ConfigObj(os.path.expanduser("~")+'/.conjurrc',configobj.pref_dict)
+            #config.read(os.path.expanduser("~")+'/.conjurrc')
+            #return config.get('Conjur', 'account')[0]
+            return config.get('account')
+        except Exception,e:
+            raise Exception("Need valid conjurrc: %s" % (e))
+            #config.read('conjur.py.example')
+            #return config.get('Conjur', 'account')[0]
 
-	def getUrl(self):
-		config = ConfigParser.ConfigParser()
-		try:
-			config.read(os.path.expanduser("~")+'/.conjurrc')
-			return config.get('Conjur', 'url')
-		except Exception,e:
-			config.read('conjur.py.example')
-			return config.get('Conjur', 'url')
-
-        def getCas(self):
-                config = ConfigParser.ConfigParser()
-                try:
-                        config.read(os.path.expanduser("~")+'/.conjurrc')
-                        return config.get('Conjur', 'cas')
-                except Exception,e:
-                        config.read('conjur.py.example')
-                        return config.get('Conjur', 'cas')
+    def getUrl(self):
+        #config = ConfigParser.ConfigParser()
+        try:
+            config = load(file(os.path.expanduser("~")+'/.conjurrc','r'))
+            #config.read(os.path.expanduser("~")+'/.conjurrc')
+            #return config.get('Conjur', 'account')[0]
+            return config.get('url')
+        except Exception,e:
+            raise Exception("Need valid conjurrc")
+            #config.read('conjur.py.example')
+            #return config.get('Conjur', 'account')[0]
+    
+    def getCas(self):
+        #config = ConfigParser.ConfigParser()
+        try:
+            config = load(file(os.path.expanduser("~")+'/.conjurrc','r'))
+            #config.read(os.path.expanduser("~")+'/.conjurrc')
+            #return config.get('Conjur', 'account')[0]
+            return config.get('cas')
+        except Exception,e:
+            raise Exception("Need valid conjurrc")
+            #config.read('conjur.py.example')
+            #return config.get('Conjur', 'account')[0]
+                       
+    def getStack(self):
+        #config = ConfigParser.ConfigParser()
+        try:
+            config = load(file(os.path.expanduser("~")+'/.conjurrc','r'))
+            #config.read(os.path.expanduser("~")+'/.conjurrc')
+            #return config.get('Conjur', 'account')[0]
+            return config.get('stack')
+        except Exception,e:
+            raise Exception("Need valid conjurrc")
+            #config.read('conjur.py.example')
+            #return config.get('Conjur', 'account')[0]
 
 
 #this logs-in and gets the service token for cas and returns the apikey
 def login_cas(username,passwd,casurl=None):
-	cfg = ConjurConfig()
-	if casurl==None:
-		casurl = cfg.getCas()
-	try:
-		params = urllib.urlencode({'username': username, 'password': passwd})
-		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-		conn = httplib.HTTPSConnection(casurl,443)
-		conn.request("POST", "/v1/tickets/", params, headers)
-		response = conn.getresponse()
-		data = response.read()
-		location = response.getheader('location')
-		if not location:
-			raise Exception("Didn't get anything back from CAS request. Username or Password wrong?")
-		#  Pull off the TGT from the end of the location, this works for CAS 3.3-FINAL
-		tgt = location[location.rfind('/') + 1:]
-		conn.close()
-		#return tgt
-		service  = 'https://authn-%s-conjur.herokuapp.com/users/login' % (cfg.getAccount())
-		params = urllib.urlencode({'service': service })
-		conn = httplib.HTTPSConnection(casurl,443)
-		conn.request("POST", "/v1/tickets/%s" % ( tgt ), params, headers)
-		response = conn.getresponse()
-		st = response.read()
-		conn.close()
+    cfg = ConjurConfig()
+    if casurl==None:
+        casurl = cfg.getCas()
+    try:
+        params = urllib.urlencode({'username': username, 'password': passwd})
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+        conn = httplib.HTTPSConnection(casurl,443)
+        conn.request("POST", "/v1/tickets/", params, headers)
+        response = conn.getresponse()
+        data = response.read()
+        location = response.getheader('location')
+        if not location:
+            raise Exception("Didn't get anything back from CAS request. Username or Password wrong?")
+        #  Pull off the TGT from the end of the location, this works for CAS 3.3-FINAL
+        tgt = location[location.rfind('/') + 1:]
+        conn.close()
+        #return tgt
+        service  = 'https://authn-%s-conjur.herokuapp.com/users/login' % (cfg.getAccount())
+        params = urllib.urlencode({'service': service })
+        conn = httplib.HTTPSConnection(casurl,443)
+        conn.request("POST", "/v1/tickets/%s" % ( tgt ), params, headers)
+        response = conn.getresponse()
+        st = response.read()
+        conn.close()
 
-		#print "service: %s" % (service)
-		#print "service token     : %s" % (st)
-		#print "***"
-		bodyurl =  "%s?ticket=%s" % ( service, st )
-		print bodyurl
-		cj = cookielib.CookieJar()
-		# no proxies please
-		no_proxy_support = urllib2.ProxyHandler({})
-		# we need to handle session cookies AND redirects
-		cookie_handler = urllib2.HTTPCookieProcessor(cj)
+        #print "service: %s" % (service)
+        #print "service token     : %s" % (st)
+        #print "***"
+        bodyurl =  "%s?ticket=%s" % ( service, st )
+        cj = cookielib.CookieJar()
+        # no proxies please
+        no_proxy_support = urllib2.ProxyHandler({})
+        # we need to handle session cookies AND redirects
+        cookie_handler = urllib2.HTTPCookieProcessor(cj)
 
-		opener = urllib2.build_opener(no_proxy_support, cookie_handler, urllib2.HTTPHandler(debuglevel=1))
-		urllib2.install_opener(opener)
-		protected_data = urllib2.urlopen(bodyurl).read()
-		#print protected_data				
-		return protected_data
-	except Exception,exc:
-		print "CAS login error: %s" % (exc)
-		return None
-
+        opener = urllib2.build_opener(no_proxy_support, cookie_handler, urllib2.HTTPHandler(debuglevel=1))
+        urllib2.install_opener(opener)
+        protected_data = urllib2.urlopen(bodyurl).read()
+        #print protected_data                
+        return protected_data
+    except Exception,exc:
+        print "CAS login error: %s" % (exc)
+        return None
+       
 #pass api key and username to get a token. Token is usually base64.encodestring'd and put in headers.
 def authenticate(username,apikey):
-        cfg = ConjurConfig()
-
-	try:
-		res = Resource('https://authn-%s-conjur.herokuapp.com' % (cfg.getAccount()))
-                url = "/users/%s/authenticate" % username
-                params = {}
-                headers = {"Accept": "text/plain"}
-		r=res.post(url,payload=apikey,headers=headers,params_dict=params)
-		return r.body_string()
-	except Exception,exc:
-		print "Error authenticate: %s" % (exc)
-
+    cfg = ConjurConfig()
+    try:
+        res = Resource('https://authn-%s-conjur.herokuapp.com' % (cfg.getAccount()))
+        url = "/users/%s/authenticate" % username
+        params = {}
+        headers = {"Accept": "text/plain"}
+        r=res.post(url,payload=apikey,headers=headers,params_dict=params)
+        return r.body_string()
+    except Exception,exc:
+        print "Error authenticate: %s" % (exc)
+        
+def tokenHandler(token):
+    token_encode = base64.encodestring(token)
+    return "Token token=\"%s\" " % token_encode
--- src/testscript.py
+++ src/testscript.py
@@ -1,5 +1,6 @@
 import sys, getopt
-from conjurapi.login import login_cas,authenticate
+from conjurapi.login import login_cas,authenticate,tokenHandler
+from conjurapi import Resource
 import base64
 
 def main(argv):
@@ -7,7 +8,7 @@ def main(argv):
     password=""
     url = ""
     try:
-        opts, args = getopt.getopt(argv,"hnu:p:r:l",["username=","password=","url=","login"])
+        opts, args = getopt.getopt(argv,"hnu:p:r:lm",["username=","password=","url=","login","permittedroles"])
     except getopt.GetoptError:
         print "To test login:"
         print 'testscript.py -u casusername -p caspassword -r cashost --login'
@@ -16,6 +17,7 @@ def main(argv):
         print '--url cashost'
         print 'Functions:'
         print '--login'
+        print '--permittedroles'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -25,6 +27,7 @@ def main(argv):
             print '--url cashost'
             print 'Functions:'
             print '--login'
+            print '--permittedroles'
             sys.exit()
         elif opt in ("-u", "--username"):
             username = arg
@@ -40,10 +43,19 @@ def main(argv):
         if opt in (" ", "--login"):
             print "Testing login: Getting CAS token"
             try:
-		apikey = login_cas(username,password,url)
+                apikey = login_cas(username,password,url)
                 print "Conjur secret %s" % apikey
-		token_encode = base64.encodestring(authenticate(username,apikey))
-		print "Authorization: Token token=\"%s\" " % token_encode
+                token_encode = tokenHandler(authenticate(username,apikey))
+                print "Authorization: %s" % token_encode
+            except Exception,exc:
+                print "Error in test: %s" % (exc)
+        elif opt in(" ","--permittedroles"):
+            try:
+                apikey = login_cas(username,password,url)
+                token_encode = tokenHandler(authenticate(username,apikey))
+                options={}
+                res = Resource()
+                res.premitted_roles(token_encode, permission, options)
             except Exception,exc:
                 print "Error in test: %s" % (exc)
         else:
