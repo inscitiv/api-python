3e69c29fdc5974e4f96e4cf2b4b9ac3643790402
bernzbroad <dbernick@broadinstitute.org>
2013-08-15 14:58:30 -0400

    all_roles to show all_roles and example in testscript.

--- src/conjurapi/Resource.py
+++ src/conjurapi/Resource.py
@@ -3,30 +3,20 @@ Created on Jul 7, 2013
 
 @author: bernz
 '''
-import ConfigParser,caslib,os.path,httplib, urllib, urllib2, cookielib
-from restkit import Resource
+import ConfigParser,os.path,httplib, urllib, urllib2, cookielib,json
+from restkit import request
 from .login import ConjurConfig
 
 class Resource:
-
-    def permitted_roles(self, token_encode, options = {}):
+    
+    def all_roles(self,token_encode,options={}):
         cfg = ConjurConfig()
         try:
-            res = Resource('https://authn-%s-conjur.herokuapp.com' % (cfg.getStack()))
-            #url = "/users/%s/authenticate" % username
-            url = "/%s/roles/allowed_to/%s/%s/%s" % (cfg.getAccount(),
-                                                     options["permission"],
-                                                     options["kind"],
-                                                     options["identifier"])
-            params = {}
-            headers = {"Accept": "text/plain",
-                       "Authorization":token_encode}
-            r=res.get(url,headers=headers,params_dict=params)
-            return r.body_string()
+            gurl = 'https://authz-%s-conjur.herokuapp.com' % (cfg.getStack())
+            headers = {"Authorization":token_encode}            
+            url = "/%s/roles/user/%s/?all" % (cfg.getAccount(),options["username"])
+            r = request(gurl+url, method='GET', headers=headers)
+            jsonenv =  json.loads(r.body_string())
+            return jsonenv
         except Exception,exc:
-            print "Error permitted: %s" % (exc)
-        
-        #JSON.parse 
-        #RestClient::Resource.new(Conjur::Authz::API.host, self.options)
-        #["#{account}/roles/allowed_to/#{permission}/#{path_escape kind}/#{path_escape identifier}"]
-        #.get(options)
+            print "Error roles: %s" % (exc)
--- src/testscript.py
+++ src/testscript.py
@@ -1,6 +1,6 @@
 import sys, getopt
 from conjurapi.login import login_cas,authenticate,tokenHandler
-from conjurapi import Resource
+from conjurapi.Resource import Resource
 from conjurapi.clilib import cliauthenticate,clilogin
 import base64
 
@@ -47,21 +47,27 @@ def main(argv):
                 casurl = url
                 clilogin(username,password,casurl)
                 token = cliauthenticate()
-                print "Logged in and Authenticated"
+                print "Logged in and Authenticated: %s" % (token)
             except Exception,exc:
                 print "Error in test: %s" % (exc)
         elif opt in(" ","--permittedroles"):
             try:
                 token = cliauthenticate()
                 options={
-                         "permission":"read",
-                         "kind":"environment",
-                         "identifier":""
+                         "username":username
                 }
                 res = Resource()
-                res.premitted_roles(token, options)
+                allroles = res.all_roles(token, options)
+                for role in allroles:
+                    tokens = role["id"].split(':')[1].split("/")
+                    try:
+                        if tokens[3]=="workspaces" and tokens[5]=="upload":
+                            print ("%s %s")%(tokens[3],tokens[5])
+                        continue
+                    except Exception,exc:
+                        continue
             except Exception,exc:
-                print "Error in test: %s" % (exc)
+                print "Error in permitted: %s" % (exc)
         else:
             print 'need a function to test (like --login)'
             print 'try -h to see options'
